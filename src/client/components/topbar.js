233a42ba86d1d15087d5d3851a6933c510dc88ee
dachtman <dachtman@gmail.com>
2018-05-20 17:42:06 -0500

    Changed to only show minute intervals

--- src/client/components/topbar.js
+++ src/client/components/topbar.js
@@ -6,6 +6,6 @@ import AppBar from 'material-ui/AppBar';
 //look at setting title to all day event if one is present
 //EX Birthday
 //Multiple BDays can rotate
-const TopBar = () => <AppBar title={<Moment format="MMM Do YYYY h:mm:ss a" interval={1000}></Moment>} showMenuIconButton={false}></AppBar>
+const TopBar = () => <AppBar title={<Moment format="MMM Do YYYY h:mm a" interval={60000}></Moment>} showMenuIconButton={false}></AppBar>
 
 export default TopBar;
